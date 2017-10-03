0a4743fe3a5cbb88867735ecef84b591e92826eb
Jonathan Guthrie <jono@perfect.org>
2017-10-03 17:34:56 -0400

    update to perfect 3 swift 4

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-03-05 22:21:25 +0000
+// Date: 2017-10-03 21:25:08 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectSessionRedis",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Redis.git", majorVersion: 2),
-    ]
+	name: "PerfectSessionRedis",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Redis.git", majorVersion: 3),
+	]
 )
